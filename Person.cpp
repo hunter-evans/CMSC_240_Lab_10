a1ad3ad1b1eb64c99389a806a88939dcbcae22e3
Palmer Robins <probins@Palmers-MacBook-Pro.local>
2018-04-02 13:51:57 -0400

    Creation of Person.cpp

---
+++ Person.cpp
@@ -0,0 +1,11 @@
+#ifndef __PERSON_CPP___
+
+#include "Person.h"
+
+Person::Person() {}
+Person::Person(const Person& other) {}
+Person::Person(int urid, std::string netid, std::string lname, std::string fname, 
+               int dob_day, int dob_month, int dob_year,
+               std::string email, std::string address, long phone)
+Person::~Person() {}
+
