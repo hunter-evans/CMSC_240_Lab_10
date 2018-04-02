2f06b7e6e281fa92e4392e5ffa8be0c8496b46e3
hunter-evans <hunter.evans@richmond.edu>
2018-04-02 14:03:57 -0400

    Added tester.cpp

--- Student.cpp
+++ Student.cpp
@@ -5,4 +5,89 @@
 #include "Person.h"
 #include "Student.h"
 
+Student::Student() {
 
+}
+
+Student::Student(const Student& other) {
+
+}
+
+Student::Student(int urid, std::string netid, std::string lname, std::string fname,
+               int dob_day, int dob_mo, int dob_yr,
+               std::string email, std::string address, long phone,
+               int day_admit, int month_admit, int year_admit,
+               School school, bool is_full_time,
+               double units_completed) {
+
+}
+
+Student::~Student() {
+
+}
+
+std::list<std::string> getCourses() {
+
+}
+
+void addCourse(std::string course) {
+
+}
+
+void removeCourse(std::string course) {
+
+}
+
+void printCourses() {
+
+}
+
+void setCourses(std::list<std::string> courses) {
+
+}
+
+void clearCourses() {
+
+}
+
+struct tm getAdmitDate() {
+  return null;
+}
+
+School getSchool() {
+  return Student::AS;
+}
+
+double getGPA() {
+  return 0.0;
+}
+
+double getUnitsCompleted() {
+  return 0.0;
+}
+
+bool isFullTime() {
+  return false;
+}
+
+void setAdmitDate(int day, int month, int year) {
+
+}
+
+void setSchool(School school) {
+
+}
+
+void setGPA(double gpa) {
+
+}
+
+void setUnitsCompleted(double units) {
+
+}
+
+void setFullTimeStatus(bool isFullTime) {
+
+}
+
+#endif
---
+++ tester.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Person.h"
+#include "Student.h"
+
+int main() {
+  //Person tests
+  std::cout << "========Beginning Person tests========" << std::endl;
+
+
+  //Student tests
+  std::cout << "========Beginning Student tests========" << std::endl;
+}
