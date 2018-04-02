a71ba39017e2d73e30217538dfbde1e2aeade2c2
Palmer Robins <probins@Palmers-MacBook-Pro.local>
2018-04-02 14:48:00 -0400

    First tests completed for Person

--- tester.cpp
+++ tester.cpp
@@ -3,8 +3,25 @@
 #include "Student.h"
 
 int main() {
+  Person p(29977710, "wr5gk", "Freeman", "Freddie", 7, 4, 1996, "ffreeman@braves.com", "2 Hank Aaron Drive", 8043600327); 
+
+
   //Person tests
   std::cout << "========Beginning Person tests========" << std::endl;
+
+  std::cout << std::endl;
+
+  //Get First Name
+  std::cout << "Testing getFirstName. Should equal 'Freddie': " << std::endl;
+  std::string fname = p.getFirstName();
+  std::cout << fname << std::endl;
+
+
+
+
+
+  std::cout << std::endl;
+
 
 
   //Student tests
