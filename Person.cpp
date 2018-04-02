9e3c5219e7d1d329eebb7122501def18f6f6b439
Palmer Robins <probins@Palmers-MacBook-Pro.local>
2018-04-02 14:05:53 -0400

    Created Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,4 +1,5 @@
-#ifndef __PERSON_CPP___
+#ifndef __PERSON_CPP__
+#define __PERSON_CPP__
 
 #include "Person.h"
 
@@ -8,4 +9,90 @@ Person::Person(int urid, std::string netid, std::string lname, std::string fname
                int dob_day, int dob_month, int dob_year,
                std::string email, std::string address, long phone)
 Person::~Person() {}
+
+
+//GETTER FUNCTIONS
+std::string Person::getFirstName() {
+	return ""; 
+}
+
+std::string Person::getLastName() {
+	return "";
+}
+
+struct tm Person::getDateOfBirth() {
+	return 0;
+}
+
+std::string Person::getAddress() {
+	return "";
+}
+
+std::string Person::getNetID() {
+	return "";
+}
+
+int Person::getURID() {
+	return 0;
+}
+
+std::string Person::getEmail() {
+	return "";
+}
+
+long Person::getPhone() {
+	return 0;
+}
+
+//SETTERS
+void Person::setFirstName(std::string fname) {
+
+}
+
+void Person::setLastName(std::string lname) {
+
+}
+
+void Person::setDateOfBirth(int month, int day, int year) {
+
+}
+
+void Person::setAddress(std::string address) {
+
+}
+
+void Person::setURID(std::string urid) {
+
+}
+
+void Person::setNetID(std::string netid) {
+
+}
+
+void Person::setEmail(std::string email) {
+
+}
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
